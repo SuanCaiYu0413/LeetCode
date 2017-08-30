410b2b3e4361050ca5395ed020e59dbef1cd5fc3
suancaiyu <2011003040@qq.com>
2017-08-30 11:51:20 +0800

    最长子串不重复字符

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -1,4 +1,26 @@
 # -*- coding:utf-8 -*-
+
+'''
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+'''
+
+'''
+    难度:中
+    1.遍历字符串,找到当前所在字符加入到当前字符串变量
+    2.遍历当前字符串后面的字符,判断下一个字符是否在当前字符串中,为假则将字符添加到当前字符串,计数+1
+    3.否则退出二层循环,判断最大长度子字符串是否小于计数器,是则将计数器值赋值给max_len,清空计数器,遍历下一组
+
+    注:在LeetCode上提交失败了,但是自我感觉是对的!!!
+'''
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         """
